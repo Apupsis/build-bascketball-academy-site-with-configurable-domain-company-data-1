e2825af1647c8245cbde8ec24004a906b63de43c
Apupsis <anubis.pupsick@gmail.com>
2026-04-16 09:37:38 +0300

    add

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  images: {
+    unoptimized: true,
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 };
 
 export default nextConfig;
